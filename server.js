4edecc4cb31f6516945858031c16c819d5e4af02
Ruslan Makeev <dec32m@gmail.com>
2019-06-14 11:44:47 +1200

    templates in progress

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const hbs = require('express-handlebars')
 const userRoutes = require('./routes/users')
 
 const server = express()
+const login = require('./views/login') // make sure linked properly to login.hbs in views
 
 // Middleware
 
-server.engine('hbs', hbs({extname: 'hbs'}))
+server.engine('hbs', hbs({extname: '.hbs'}))
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({extended: true}))
 
