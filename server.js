e33f2206fb58e8b88e59d9b60387a5faa27839c0
Ruslan Makeev <dec32m@gmail.com>
2019-06-14 11:56:45 +1200

    changes added

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const hbs = require('express-handlebars')
 const userRoutes = require('./routes/users')
 
 const server = express()
-const login = require('./views/login') // make sure linked properly to login.hbs in views
 
 // Middleware
 
-server.engine('hbs', hbs({extname: '.hbs'}))
 server.set('view engine', 'hbs')
 server.use(express.urlencoded({extended: true}))
 
